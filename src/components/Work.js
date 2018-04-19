eb230b83fe2f60a1bf987dfe2061001226e14cb7
olegchursin <olegchursin@gmail.com>
2018-04-19 14:25:46 -0400

    correct links in Work component

--- src/components/Work.js
+++ src/components/Work.js
@@ -11,13 +11,13 @@ const Work = (props) => {
       <MiniDivider />
       <div className='portfolio'>
         <div className='portfolio-item' id='portfolio-la'>
-          <a href="https://linguistic-analysis.herokuapp.com/">linguistic analysis</a>
+          <a href="https://linguistic-analysis.herokuapp.com/" target="_blank">linguistic analysis</a>
         </div>
-        <div className='portfolio-item' id='portfolio-islahook'><a href="https://olegchursin.myportfolio.com/islahook-landing-page">Isla+Hook</a></div>
-        <div className='portfolio-item' id='portfolio-bbar'><a href="http://bbarandgrill.com">BBar & Grill</a></div>
-        <div className='portfolio-item' id='portfolio-lafayette'><a href="http://lafayettehousenyc.com/">Lafayette House</a></div>
-        <div className='portfolio-item' id='portfolio-ryan'><a href="https://olegchursin.myportfolio.com/ryan-cunningham-personal-brand">Ryan Cunningham</a></div>
-        <div className='portfolio-item' id='portfolio-dlaw'><a href="http://new.dzhamgarovalaw.com/en/index.html">D Law</a></div>
+        <div className='portfolio-item' id='portfolio-islahook'><a href="https://www.behance.net/gallery/63431973/ISLAHOOK-Landing-page" target="_blank">Isla+Hook</a></div>
+        <div className='portfolio-item' id='portfolio-bbar'><a href="http://bbarandgrill.com" target="_blank">BBar & Grill</a></div>
+        <div className='portfolio-item' id='portfolio-lafayette'><a href="http://lafayettehousenyc.com/" target="_blank">Lafayette House</a></div>
+        <div className='portfolio-item' id='portfolio-ryan'><a href="https://olegchursin.myportfolio.com/ryan-cunningham-personal-brand" target="_blank">Ryan Cunningham</a></div>
+        <div className='portfolio-item' id='portfolio-dlaw'><a href="http://new.dzhamgarovalaw.com/en/index.html" target="_blank">D Law</a></div>
       </div>
       <Divider />
     </div>
